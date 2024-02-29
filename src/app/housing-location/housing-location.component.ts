43692ebae5054f46d693a4d7bc50b1017ad35a32
ANNARITA PASCAZIO <apascazi@emeal.nttdata.com>
2024-02-29 10:57:11 +0100

    Modifiche 23

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,6 +1,6 @@
 import { Component,Input} from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Housinglocation } from '../home/housinglocation';
+import { Housinglocation } from './housinglocation';
 
 
 @Component({
