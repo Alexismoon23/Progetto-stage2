c084e0c15f7895654fe26825808402b3a24007ac
ANNARITA PASCAZIO <apascazi@emeal.nttdata.com>
2024-03-07 17:14:59 +0100

    json

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -14,15 +14,15 @@ import { HousingService } from '../housing.service';
   <p class="green">Homes</p>
     <section>
       <form>
-        <input type="text" placeholder="Filter by city">
-        <button class="primary" type="button">Search</button>
+        <input type="text" placeholder="Filter by city" #filter>
+        <button class="primary" type="button" (click)="filterResults(filter.value)">Search</button>
       </form>
     </section>
     <section class="results">
       <app-housing-location
-        *ngFor="let housingLocation of housingLocationList"
-        [housingLocation]="housingLocation">
-      </app-housing-location>
+        *ngFor="let housingLocation of filteredLocationList" [housingLocation]="housingLocation"></app-housing-location>
+        
+      
     </section>
   `,
   styleUrls: ['./home.component.css'],
@@ -36,5 +36,14 @@ constructor() {
   this.housingService.getAllHousingLocations();
   this.filteredLocationList= this.housingLocationList;
 }
+filterResults(text:string){
+  if(!text){
+this.filteredLocationList = this.housingLocationList;
+return;
+  }
+this.filteredLocationList = this.housingLocationList.filter(
+  housingLocation => housingLocation?.city.toLowerCase().includes(text.toLowerCase()),
+);
 
+  }
 }
